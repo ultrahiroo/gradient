0ac93abb230ed7e421f115d1125e1df552a30d98
Inoue Hiroo <ultrahiroo2000@yahoo.co.jp>
2023-01-18 05:40:47 +0000

    WIP

--- init_yash.sh
+++ init_yash.sh
@@ -11,7 +11,7 @@ alias e="${EDITOR}"
 alias edit="${EDITOR}"
 alias py='python'
 
-VENV_ACTIVATE='/notebooks/.venv/bin/activate'
+VENV_ACTIVATE='/notebooks/.env/bin/activate'
 
 if test -e ${VENV_ACTIVATE}; then
     . ${VENV_ACTIVATE}
@@ -20,7 +20,7 @@ fi
 export OLDPYTHONPATH="${PYTHONPATH}"
 export PYTHONPATH="${OLDPYTHONPATH}"
 export PYTHONPATH="/usr/local/lib/python3.8/dist-packages:${PYTHONPATH}"
-export PYTHONPATH="/notebooks/.venv/lib/python3.8/site-packages:${PYTHONPATH}"
+export PYTHONPATH="/notebooks/.env/lib/python3.8/site-packages:${PYTHONPATH}"
 
 export COLORTERM='truecolor'
 export XDG_CONFIG_HOME='/notebooks/setting'
