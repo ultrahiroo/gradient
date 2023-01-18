ddae63534bd74fdb80a6172eeaefc685613dc3a9
Inoue Hiroo <one@localhost>
2023-01-18 12:09:30 +0900

    WIP

--- setup.sh
+++ setup.sh
@@ -1,2 +1,29 @@
-#!/bin/sh
-jupyter notebook --allow-root --ip=0.0.0.0 --no-browser --NotebookApp.trust_xheaders=True --NotebookApp.disable_check_xsrf=False --NotebookApp.allow_remote_access=True --NotebookApp.allow_origin='*'
+#!/bin/bash
+DETECTED_NUMBER_OF_IPUS=$(python .gradient/available_ipus.py)
+
+if [[ "$1" == "test" ]]; then
+    IPU_ARG="${DETECTED_NUMBER_OF_IPUS}"
+else
+    IPU_ARG=${1:-"${DETECTED_NUMBER_OF_IPUS}"}
+fi
+
+export NUM_AVAILABLE_IPU=${IPU_ARG}
+export GRAPHCORE_POD_TYPE="pod${IPU_ARG}"
+export POPLAR_EXECUTABLE_CACHE_DIR="/tmp/exe_cache"
+export DATASET_DIR="/tmp/dataset_cache"
+export CHECKPOINT_DIR="/tmp/checkpoints"
+
+export PUBLIC_DATASET_DIR="/datasets"
+
+export TF_POPLAR_FLAGS='--executable_cache_path='${POPLAR_EXECUTABLE_CACHE_DIR}''
+
+export OGB_DATASET_DIR="${DATASET_DIR}/ogb_lsc_pcqm4mv2/datasets"
+export OGB_CHECKPOINT_DIR="${CHECKPOINT_DIR}/ogb_lsc_pcqm4mv2/checkpoints"
+export OGB_SUBMISSION_CODE="./pcqm4mv2_submission"
+
+nohup /notebooks/.gradient/prepare-datasets.sh ${@} & tail -f nohup.out &
+
+export PIP_DISABLE_PIP_VERSION_CHECK=1 CACHE_DIR=/tmp
+jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True \
+            --ServerApp.disable_check_xsrf=False --ServerApp.allow_remote_access=True \
+            --ServerApp.allow_origin='*' --ServerApp.allow_credentials=True
