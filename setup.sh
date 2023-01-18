b7db01fa04d8b8442c58e13eb43f944b1eb4b103
Inoue Hiroo <one@localhost>
2023-01-18 11:46:15 +0900

    WIP

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-sh init_apt.sh
-echo /notebooks/.usr/lib > /etc/ld.so.conf.d/notebooks.conf
-ldconfig
-yash
+# sh init_apt.sh
+# echo /notebooks/.usr/lib > /etc/ld.so.conf.d/notebooks.conf
+# ldconfig
+# yash
+env
