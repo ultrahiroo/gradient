8701e73366a1a04afa2f43910b26db3af96c830b
Inoue Hiroo <one@localhost>
2023-02-15 05:39:27 +0900

    WIP

--- graphcore_ipu_number.py
+++ graphcore_ipu_number.py
@@ -1,9 +1,4 @@
 import subprocess
 import json
 
-j = subprocess.check_output(['gc-monitor', '-j'])
-data = json.loads(j)
-num_ipuMs = len(data['cards'])
-num_ipus = 4 * num_ipuMs
-
-print(num_ipus)
+print(4 * len(json.loads(subprocess.check_output(['gc-monitor', '-j']))['cards']))
