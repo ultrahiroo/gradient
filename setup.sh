26decd7642bf28bf6933b988e9b7cae81e062de4
Inoue Hiroo <one@localhost>
2023-02-18 04:19:50 +0900

    WIP

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 nohup git -C /notebooks/ pull
 nohup sh /notebooks/init_apt.sh
 
+nohup git -C /notebooks/trade/ pull
+
 export DATASET_DIR="/tmp/dataset_cache"
 export CHECKPOINT_DIR="/tmp/checkpoints"
 export PUBLIC_DATASET_DIR="/datasets"
