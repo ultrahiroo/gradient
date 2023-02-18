7487a4dbb3f3bdbe68c1476323448d465b568327
Inoue Hiroo <one@localhost>
2023-02-18 16:02:40 +0900

    WIP

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ export PUBLIC_NOTEBOOK_DIR='/notebooks'
 
 export PIP_DISABLE_PIP_VERSION_CHECK='1'
 export CACHE_DIR='/tmp'
-export SHELL='/notebooks/yash'
+# export SHELL='/notebooks/yash'
 jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True \
             --ServerApp.disable_check_xsrf=False --ServerApp.allow_remote_access=True \
             --ServerApp.allow_origin='*' --ServerApp.allow_credentials=True
