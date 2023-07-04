dec79943a3a18fb8273c128dbdfab41a360cf76d
Inoue Hiroo <one@localhost>
2023-07-05 08:28:26 +0900

    WIP

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ fi
 
 export PIP_DISABLE_PIP_VERSION_CHECK='1'
 export CACHE_DIR='/tmp'
-# export SHELL='/notebooks/command/yash'
 jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True \
             --ServerApp.disable_check_xsrf=False --ServerApp.allow_remote_access=True \
-            --ServerApp.allow_origin='*' --ServerApp.allow_credentials=True
+            --ServerApp.allow_origin='*' --ServerApp.allow_credentials=True \
+            --ServerApp.terminado_settings="shell_command=['/notebooks/command/yash']"
